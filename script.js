612956577c9e986b046e96711fee7d18fb6a787c
hulybrenda <castrohulybrenda12@gmail.com>
2025-07-04 15:55:11 -0300

    adicionar uma pagina hlml, atualizar javascript e o css

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Efeito 1: Mostrar mensagem especial no botão da página inicial
+// Mostrar mensagem especial no botão da página inicial
 document.addEventListener('DOMContentLoaded', () => {
   const btnMensagem = document.getElementById('btnMensagem');
   const mensagem = document.getElementById('mensagem');
@@ -8,5 +8,37 @@ document.addEventListener('DOMContentLoaded', () => {
       mensagem.classList.toggle('hidden');
     });
   }
+  // Validação do formulário e efeito ao enviar
+function validarFormulario() {
+  const nome = document.getElementById("nome").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const mensagem = document.getElementById("mensagemContato").value.trim();
+  const aviso = document.getElementById("msg-enviada");
+
+  if (!nome || !email || !mensagem) {
+    alert("Por favor, preencha todos os campos.");
+    return false;
+  }
+
+  aviso.classList.remove("hidden");
+  aviso.style.color = "green";
+  return false;
+}
+
+// borda muda de cor ao focar nos campos do formulário
+document.addEventListener('DOMContentLoaded', () => {
+  const inputs = document.querySelectorAll("input, textarea");
+
+  inputs.forEach(input => {
+    input.addEventListener("focus", () => {
+      input.style.border = "2px solid #600454";
+    });
+
+    input.addEventListener("blur", () => {
+      input.style.border = "1px solid #ccc";
+    });
+  });
+});
+
 });
 
